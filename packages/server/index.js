50609fdcf29a24b7c8d0a783e1fb3aaace89a9c4
Yashaswini J <yashaswinij1999>
2024-08-09 10:09:34 +0530

    connected to databse

--- packages/server/index.js
+++ packages/server/index.js
@@ -3,7 +3,18 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-const connectDb = () => {};
+const connectDb = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Yashu:Yashu123@cluster0.rbr83rj.mongodb.net/expense-tracker"
+    );
+    console.log("db connected");
+  } catch (error) {
+    console.log("failed to connect db");
+  }
+};
+
+connectDb();
 
 app.listen(5000, () => {
   console.log("server connected");
