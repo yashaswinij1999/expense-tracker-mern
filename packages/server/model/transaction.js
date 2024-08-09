a325441c354ebb3d3a8cf6cb997e6f9dccfb70c9
Yashaswini J <yashaswinij1999>
2024-08-09 10:10:08 +0530

    created transaction model

---
+++ packages/server/model/transaction.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Transaction = mongoose.model("Transactions", transactionSchema);
+module.exports = Transaction;
