a134c76189a12edcb8285660bb4abe99f3429be1
Yashaswini J <yashaswinij1999>
2024-08-10 10:29:29 +0530

    created transaction list folder

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Balance from "./components/Balance";
+import Heading from "./components/Heading";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 function App() {
   return (
     <>
+      <Heading />
       <Balance />
+      <IncomeExpenses />
     </>
   );
 }
